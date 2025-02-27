670462a9bfc3099f97ca917ab2bb01782a25f809
nzaman7878 <nzaman7878>
2025-02-27 23:54:26 +0530

    showing name in browser console

--- Basic/basic.js
+++ Basic/basic.js
@@ -1,16 +1,4 @@
-// console.log("Nuruzzaman is a software developer");
+console.log("Nuruzzaman is a software developer");
 
-// const dt = new Date();
 
-// console.log(dt.getFullYear());
-
-// var first = "Nuruz";
-// var last = "Zaman";
-
-// console.log(first + " " + last);
-
-var a = 10;
-console.log(a);
-var a = 20;
-console.log(a);
 

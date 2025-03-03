587eb11de13c294d7b8c98a4dda879590d52e2bb
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-03 23:19:15 +0530

    Reassign a value to a variable declared with let and log the result

--- variable and data types/script.js
+++ variable and data types/script.js
@@ -8,6 +8,13 @@
 
 // 2 . create a constant to store the  value value of PI and log it
 
-const PI = Math.PI;
+// const PI = Math.PI;
 
-console.log(PI);
+// console.log(PI);
+
+// 3. Reassign a value to a variable declared with let and log the result.
+
+let b= 12;
+b = 32;
+
+console.log(b);

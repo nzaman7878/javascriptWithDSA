47e2ebfb3c4aa3078576e21b1f639fbcd853b864
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-03 23:16:32 +0530

    create a constant to store the  value value of PI and log it

--- variable and data types/script.js
+++ variable and data types/script.js
@@ -2,6 +2,12 @@
 
 // 1. Declare a variable using let and log its value
 
-let aa = 12;
+// let aa = 12;
 
-console.log(aa);
+// console.log(aa);
+
+// 2 . create a constant to store the  value value of PI and log it
+
+const PI = Math.PI;
+
+console.log(PI);

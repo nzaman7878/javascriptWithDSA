c38c2e1c2d24743388f54c8d44151a5a5a8abc7b
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-03 23:04:35 +0530

    create a variable holding your age wheter its greater than 18

--- Basic/basic.js
+++ Basic/basic.js
@@ -1,15 +1,22 @@
-// console.log("Nuruzzaman is a software developer");
+// // console.log("Nuruzzaman is a software developer");
 
-// const dt = new Date();
+// // const dt = new Date();
 
-// console.log(dt.getFullYear());
+// // console.log(dt.getFullYear());
 
-// var first = "Nuruz";
-// var last = "Zaman";
+// // var first = "Nuruz";
+// // var last = "Zaman";
 
-// console.log(first + " " + last);
+// // console.log(first + " " + last);
 
-var a = 10;
-console.log(a);
-var a = 20;
-console.log(a);
+// var a = 10;
+// console.log(a);
+// var a = 20;
+// console.log(a);
+
+// create a variable holding your age wheter its greater than 18
+var age = 23;
+
+if (age > 18)
+    console.log(true)
+else console.log(false)

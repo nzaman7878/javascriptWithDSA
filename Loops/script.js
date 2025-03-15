8cc68b0c7751fa751a88155bae4ed20599826c59
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-15 23:41:22 +0530

    use a while loop to sum the sum the number from 1 to 10

--- Loops/script.js
+++ Loops/script.js
@@ -2,6 +2,17 @@
 
 //1.Write a for loop to print numbers from 1 to 50
 
-for (let i = 1; i <= 50; i++) {
-    console.log(i);
-  }
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i);
+//   }
+
+//use a while loop to sum the sum the number from 1 to 10
+
+let a = 1;
+let sum = 0;
+ while (a <=11) {
+    sum = sum + a;
+    a++;
+ }
+
+ console.log(sum);

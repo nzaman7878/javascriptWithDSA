4cae89bc17c26a42a89faa5f91597d52a6d35a5e
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-17 02:38:09 +0530

    Remove the last elements of an array and log the updated array

--- Array/script.js
+++ Array/script.js
@@ -12,6 +12,12 @@
 
 //Add two new elements to the start of an array using .unshift()
 
-var arr=[1,2,3,3];
-arr.unshift(0);
-arr.unshift(-1);
+// var arr=[1,2,3,3];
+// arr.unshift(0);
+// arr.unshift(-1);
+
+//Remove the last elements of an array and log the updated array
+
+var arr = [1,2,3,4,5];
+arr.pop();
+console.log(arr);

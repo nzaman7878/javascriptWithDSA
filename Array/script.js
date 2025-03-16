48937adac6975e43bb42bd901836930011d2b2a4
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-03-17 02:32:55 +0530

    Find and log the second elements of an array

--- Array/script.js
+++ Array/script.js
@@ -1,6 +1,11 @@
 //Create an array of your top 5 favorite movies and log it
 
-var arr = ["The Godfather", "The Dark Knight", "The ShawsShank Redemption", "The Godfather II", "The Lion KIng"];
-arr.forEach(function(movie){
-    console.log(movie);
-})
+// var arr = ["The Godfather", "The Dark Knight", "The ShawsShank Redemption", "The Godfather II", "The Lion KIng"];
+// arr.forEach(function(movie){
+//     console.log(movie);
+// })
+
+//Find and log the second elements of an array
+
+var arr = [1,2,3,4,5];
+console.log(arr[1]);
